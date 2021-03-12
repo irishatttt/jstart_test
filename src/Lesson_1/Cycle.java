69d45ae5455175a570987a6970a3c36fcf514ae2
Irina Ageykina <irishatttt@mail.ru>
2021-03-12 11:33:24 +0300

    add Cycle

---
+++ src/Lesson_1/Cycle.java
@@ -0,0 +1,30 @@
+package Lesson_1;
+
+public class Cycle {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+/*        int i = 6;
+        int result = 2;
+        while (i >= -6) {
+            if (i % 2 == 0) {
+                result = result + i;
+                System.out.println(i);
+            }
+            i--;
+        }*/
+
+      /*  int i = 10;
+        int result = 0;
+        do {
+            if (i % 2 != 0) {
+                result = result + i;
+            }
+            i++;
+        } while (i <= 20);
+        System.out.println(result);*/
+    }
+}
